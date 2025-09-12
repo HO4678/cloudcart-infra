542d5d3d7dcdedc5baaf18c3d906af35ad459c28
Habib Ojoye <ojoyehabib2@gmail.com>
2025-09-11 20:59:39 -0400

    Commit change

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "ALB DNS"
-  value       = aws_lb.alb.dns_name
-}
-
-output "rds_endpoint" {
-  description = "RDS endpoint"
-  value       = aws_db_instance.postgres.address
-}
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-docker_image = "nginxdemos/hello:latest"
-db_username  = "ho562"
-db_password  = "Playstation@64"
-aws_region   = "us-east-1"
